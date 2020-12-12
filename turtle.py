50d228fe4ddb3258f5e56f7ab4d7003895eeec01
MrNobody326 <75765023+MrNobody326@users.noreply.github.com>
2020-12-11 21:44:17 -0800

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,11 @@
+import turtle
+
+turtle.pendown()
+turtle.color("red")
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
